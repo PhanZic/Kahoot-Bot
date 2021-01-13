35e281245dcff2dcbb375b29640c90829e2a2909
PhanZic <55498246+PhanZic@users.noreply.github.com>
2021-01-13 21:26:06 +0100

    remade app.js

--- app.js
+++ app.js
@@ -1,42 +1,46 @@
+// Coded by PhanZic#0441
 const Kahoot = require("kahoot.js-updated");
+var kahoots = [];
 const fs = require('fs');
-const gameCode = 7805071;
 const path = require('path');
-const client = new Kahoot;
-var name = 'Atlib' + ' ' + Math.floor(Math.random() * 99999999).toString()
+const gameCode = 6857875;
+var name = 'CodeSec' + ' ' + Math.floor(Math.random() * 99999999).toString()
+var bot_count = 230;
 
-setInterval(kahoot, 30)
-function kahoot() {
-    client.join(gameCode, name);
-
-    client.on("Joined", () => {
-        console.log(`I joined the Kahoot! with the name ${name}`);
-        if (!fs.existsSync('Logs')) {
-            fs.mkdir(path.join(__dirname, '/Logs'),
-                { recursive: true }, (err) => {
-                    if (err) {
-                        return console.error(err);
-                    }
-                    console.log('Directory created successfully!');
+setTimeout(function () {
+    for (var i = 0; i < bot_count; i++) {
+        kahoots.push(new Kahoot);
+        kahoots[i].join(gameCode, name + " " + String(i)).catch(error => {
+            console.log("join failed " + error.description + " " + error.status)
+        });
+        kahoots[i].on("Joined", () => {
+            console.log("successfully joined game with name:" + ' ' + name)
+            if (!fs.existsSync('Logs')) {
+                fs.mkdir(path.join(__dirname, '/Logs'),
+                    { recursive: true }, (err) => {
+                        if (err) {
+                            return console.error(err);
+                        }
+                        console.log('Directory created successfully!');
+                    });
+                fs.appendFile(`${__dirname}/Logs/${gameCode}.txt`, `Bot Users: ${name}\r\n`, function (err) {
+                    if (err) throw err;
                 });
-            fs.appendFile(`${__dirname}/Logs/${gameCode}.txt`, `fucked game: ${gameCode}\r\n`, function (err) {
-                if (err) throw err;
-            });
-        } else {
-            fs.appendFile(`${__dirname}/Logs/${gameCode}.txt`, `fucked game: ${gameCode}\r\n`, function (err) {
-                if (err) throw err;
-            });
-        }
-    });
-
-    client.on("QuizStart", () => {
-    });
-
-    client.on("QuestionStart", question => {
-        question.answer(Math.floor(Math.random() * 5));
-        console.log('i answered a question')
-    });
-    client.on("QuizEnd", () => {
-        console.log("The quiz has ended.");
-    });
-}
+            } else {
+                fs.appendFile(`${__dirname}/Logs/${gameCode}.txt`, `Bot Users: ${name}\r\n`, function (err) {
+                    if (err) throw err;
+                });
+            }
+        });
+        kahoots[i].on("QuestionStart", (question) => {
+            question.answer(
+                Math.floor(
+                    Math.random() * question.quizQuestionAnswers[question.questionIndex]
+                ) + 0
+            );
+        });
+        kahoots[i].on("Disconnect", (reason) => {
+            console.log("disconnected because " + reason);
+        });
+    }
+}, 5000);
